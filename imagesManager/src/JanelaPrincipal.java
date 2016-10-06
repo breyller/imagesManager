2a0cefebbacdf93491726a8f1db3d3a247f94b12
b140528 <b140528@LI050>
2016-10-05 21:36:44 -0300

    teste

--- imagesManager/src/JanelaPrincipal.java
+++ imagesManager/src/JanelaPrincipal.java
@@ -6,7 +6,7 @@ public class JanelaPrincipal extends javax.swing.JFrame {
      * Creates new form JanelaPrincipal
      */
     public JanelaPrincipal() {
-        initComponents();
+        
     }
 
     /**
