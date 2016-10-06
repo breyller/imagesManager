11c2dc2222ac888c86e8474a109fa73ebc5ca5d6
breyller <brunoreyller@gmail.com>
2016-10-05 22:25:59 -0300

    conexao com banco, falta testar

--- imagesManager/src/DbManipulate.java
+++ imagesManager/src/DbManipulate.java
@@ -1,4 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbManipulate implements IPersistencia{
+    
+    private Connection conn;
+    
+    public DbManipulate(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3059/imagesManager", "root", "root");
+            System.out.println("Conectado com sucesso.");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erro de class not found: " + ex);
+        } catch (SQLException ex) {
+            System.out.println("Erro de conexao mysql: " + ex);
+        }
+        
+    }
     
     public Image getImageById(int id){
         Image a = new Image();
