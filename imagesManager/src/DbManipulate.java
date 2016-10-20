e9833b93bc6e03de82847c853c38904123c19a75
breyller <brunoreyller@gmail.com>
2016-10-19 22:04:37 -0200

    DbManipulate buscando foto por id

--- imagesManager/src/DbManipulate.java
+++ imagesManager/src/DbManipulate.java
@@ -1,20 +1,44 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DbManipulate implements IPersistencia{
     
-    private Connection conn;
-    
-    public DbManipulate() throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.jdbc.Driver");
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3059/imagesManager", "root", "root");
-        System.out.println("Conectado com sucesso.");
-    }
-    
     public Image getImageById(int id){
-        Image a = new Image();
-        return a;
+        Image img = null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DbConnector.getConnection();
+            
+            stmt = conn.prepareStatement("SELECT * FROM fotos WHERE idFotos = ?");
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                img = new Image();
+                img.setId(rs.getInt("idFotos"));
+                img.setTitle(rs.getString("title"));
+                img.setDescription(rs.getString("description"));
+                img.setPath(rs.getString("path"));
+                img.setHash(rs.getString("hash"));
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DbManipulate.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                DbConnector.closeConnection(conn, stmt, rs);
+            } catch (SQLException ex) {
+                Logger.getLogger(DbManipulate.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return img;
     }
 
     public Image getImageByHash(String hash) {
