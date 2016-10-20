1f1f02c5f376ef4f8688a9e558b394d2e651266c
breyller <brunoreyller@gmail.com>
2016-10-19 22:42:57 -0200

    funcao que retorna imagem pelo hash e funcao que retorna arraylist de
    imagens pelo title

--- imagesManager/src/DbManipulate.java
+++ imagesManager/src/DbManipulate.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -42,20 +43,76 @@ public class DbManipulate implements IPersistencia{
     }
 
     public Image getImageByHash(String hash) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
-    public Image getImageByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
-    public static void main(String[] args) {
-        try{
-            DbManipulate a;
-            a = new DbManipulate();
-        }catch(Exception e){
-            System.out.println("Erro ao instanciar: " + e);
+        Image img = null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DbConnector.getConnection();
+            
+            stmt = conn.prepareStatement("SELECT * FROM fotos WHERE hash = ?");
+            stmt.setString(1, hash);
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                img = new Image();
+                img.setId(rs.getInt("idFotos"));
+                img.setTitle(rs.getString("title"));
+                img.setDescription(rs.getString("description"));
+                img.setPath(rs.getString("path"));
+                img.setHash(rs.getString("hash"));
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DbManipulate.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                DbConnector.closeConnection(conn, stmt, rs);
+            } catch (SQLException ex) {
+                Logger.getLogger(DbManipulate.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+        
+        return img;
     }
+    
+    public ArrayList<Image> getImageByTitle(String title) {
+        Image img = null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Image> images = new ArrayList<>();
+        
+
+        try {
+            conn = DbConnector.getConnection();
+            
+            stmt = conn.prepareStatement("SELECT * FROM fotos WHERE title LIKE ?");
+            stmt.setString(1, "%"+title+"%");
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                img = new Image();
+                img.setId(rs.getInt("idFotos"));
+                img.setTitle(rs.getString("title"));
+                img.setDescription(rs.getString("description"));
+                img.setPath(rs.getString("path"));
+                img.setHash(rs.getString("hash"));
+                
+                images.add(img);
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DbManipulate.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                DbConnector.closeConnection(conn, stmt, rs);
+            } catch (SQLException ex) {
+                Logger.getLogger(DbManipulate.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return images;
+    }
+    
     
 }
