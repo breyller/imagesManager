857932c9bf93243f278077e2b9b8f157c63dc501
breyller <brunoreyller@gmail.com>
2016-10-26 20:32:04 -0200

    funcoes de persistencia a serem implementadas

--- imagesManager/src/IPersistencia.java
+++ imagesManager/src/IPersistencia.java
@@ -1,8 +1,25 @@
 import java.util.ArrayList;
 
 public interface IPersistencia {
-    Image getImageById(int id);
-    Image getImageByHash(String hash);
-    ArrayList<Image> getImageByTitle(String title);
     
+    Image getImageById(int id);
+    Image getImageById(int id, int idAlbum);
+    Image getImageByHash(String hash);
+    Image getImageByHash(String hash, int idAlbum);
+    ArrayList<Image> getImageByTitle(String title);
+    ArrayList<Image> getImageByTitle(String title, int idAlbum);
+    ArrayList<Image> getImageByDescription(String description);
+    ArrayList<Image> getImageByDescription(String description, int idAlbum);
+    ArrayList<Image> getImageByAlbumId(int idAlbum);
+    Album getAlbumById(int id);
+    ArrayList<Album> getAlbumByTitle(String title);
+    ArrayList<Image> getAlbumByDescription(String description);
+    boolean setAlbum(Album alb);
+    boolean setImage(Image img);
+    boolean setImageOnAlbum(Image img, int idAlbum);
+    boolean updateAlbum(Album alb);
+    boolean updateImage(Image img);
+    boolean deleteImage(Image img);
+    boolean deleteAlbum(Album alb);
+    boolean setImageToAlbum(Image img, Album alb);
 }
